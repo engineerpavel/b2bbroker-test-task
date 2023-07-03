4bd9ba1bb06f4d345f91288b1f3d6c86b35ba26b
Pavel Pogorelov <engineer.pavel@yandex.com>
2023-07-03 21:35:14 +0300

    pretty-quick

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,4 @@ import { Component } from '@angular/core';
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-    title = 'b2bbroker-test-task';
-}
+export class AppComponent {}
